2ac685b3d19c351038eb27604f189a5267163bb6
Per Karlsson <per@exoticmatter.com>
2011-08-02 19:04:19 +0200

    first backup

---
+++ common/NbF3D.h
@@ -0,0 +1,310 @@
+// ----------------------------------------------------------------------------
+//
+// NbF3D.h
+//
+// Copyright (c) 2011 Exotic Matter AB.  All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are
+// met:
+//
+// * Redistributions of source code must retain the above copyright notice,
+//    this list of conditions and the following disclaimer.
+//
+// * Redistributions in binary form must reproduce the above copyright notice,
+//   this list of conditions and the following disclaimer in the documentation
+//   and/or other materials provided with the distribution.
+//
+// * Neither the name of Exotic Matter AB nor its contributors may be used to
+//   endorse or promote products derived from this software without specific
+//   prior written permission.
+//
+//    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+//    "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,  INCLUDING,  BUT NOT
+//    LIMITED TO,  THE IMPLIED WARRANTIES OF  MERCHANTABILITY AND FITNESS
+//    FOR  A  PARTICULAR  PURPOSE  ARE DISCLAIMED.  IN NO EVENT SHALL THE
+//    COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+//    INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+//    BUT  NOT LIMITED TO,  PROCUREMENT OF SUBSTITUTE GOODS  OR  SERVICES;
+//    LOSS OF USE,  DATA,  OR PROFITS; OR BUSINESS INTERRUPTION)  HOWEVER
+//    CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,  STRICT
+//    LIABILITY,  OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)  ARISING IN
+//    ANY  WAY OUT OF THE USE OF  THIS SOFTWARE,  EVEN IF ADVISED OF  THE
+//    POSSIBILITY OF SUCH DAMAGE.
+//
+// ----------------------------------------------------------------------------
+
+//Field 3D API
+#include <Field3D/DenseField.h>
+#include <Field3D/SparseField.h>
+#include <Field3D/Field3DFile.h>
+#include <Field3D/InitIO.h>
+
+namespace NbF3D{
+
+    enum type{
+        DENSE = 0,
+        SPARSE = 1,
+        MAC = 2 //Currently not supported, maybe Batman can fix it /"\'..'/"\.
+    };
+
+    //Not used at the moment
+    class Vec3f : public Imath::V3f
+    {
+    public:
+        Vec3f(): Imath::V3f() {};
+        Vec3f(int i): Imath::V3f(i) {};
+        Vec3f(const Nb::Vec3f & v) : Imath::V3f(v[0], v[1], v[2]){};
+    };
+
+    struct Field3f{
+        const Nb::Field1f &  fU, & fV, & fW;
+        const Nb::TileLayout & tileLayout;
+
+        Imath::V3f operator()(const int i) const
+        {
+            //Get cell indices from global tile cell index i
+            int ci, cj, ck;
+            tileLayout.cijk(i, ci, cj, ck);
+
+            //Get the global coord of the other side of the MAC
+            //Will return -1 if outside fine tile layout
+            int       idu = tileLayout.cell(ci - 1, cj, ck)
+                    , idv = tileLayout.cell(ci, cj - 1, ck)
+                    , idw = tileLayout.cell(ci, cj, ck - 1);
+
+            //Float values (for example velocity)
+            float u,v,w;
+
+            //If negative index, clamp to the only valid one.
+            //If positive, mean value of the two Marker values
+            //(store in center ofCell)
+            if (idu < 0)
+                u = fU(i);
+            else
+                u = (fU(i) + fU(idu))/2;
+
+            if (idv < 0)
+                v = fV(i);
+            else
+                v = (fV(i) + fV(idv))/2;
+
+            if (idw < 0)
+                w = fW(i);
+            else
+                w = (fW(i) + fW(idw))/2;
+
+            //Imath to be safe.
+            return Imath::V3f(u,v,w);
+        };
+    };
+
+
+
+    template<class F, class T>
+    void initField( F fieldPtr
+                  , const Nb::Body * body
+                  , const int chnIdx
+                  , const Nb::Vec3i & size
+                  , const T & def)
+    {
+        //Field name after the body's name
+        fieldPtr->name = body->name().c_str();
+
+        //Access field shape
+        const Nb::FieldShape & fieldShape = body->constFieldShape();
+
+        //Attribute name after the channel name
+        fieldPtr->attribute = fieldShape.channel(chnIdx)->name().c_str();
+
+        //Bounding Box
+        fieldPtr->setSize(Imath::V3i(size[0], size[1], size[2]));
+
+        //Set default value everywhere to begin with
+        fieldPtr->clear(def);
+    };
+
+    template<class F, class nF>
+    void chanToField(F fieldPtr
+            , const Nb::Body * body
+            , const nF & naiadField
+            , const Nb::Vec3i & offset)
+    {
+        //Access field shape
+        const Nb::FieldShape & fieldShape = body->constFieldShape();
+
+        //Get Tile Layout
+        const Nb::TileLayout & tileLayout = body->constLayout();
+
+        //Cell indices
+        int ci, cj ,ck;
+
+        for (int iTiles = 0; iTiles < tileLayout.fineTileCount(); ++iTiles){
+            Nb::Tile tile(tileLayout.fineTile(iTiles));
+            //We are only interested in the fine tiles.
+            if(tile.type()==Nb::Tile::Fine) {
+                for (int iTC = tile.cellsBegin(); iTC < tile.cellsEnd(); ++iTC){
+                    tileLayout.cijk(iTC,ci,cj,ck);
+                    ci -= offset[0];
+                    cj -= offset[1];
+                    ck -= offset[2];
+                    fieldPtr->fastLValue(ci, cj, ck) = naiadField(iTC);
+                }
+            }
+        }
+    };
+
+    template <bool scalar, class F, class T>
+    class F3DFileWrite{
+    public:
+        static void write(Field3D::Field3DOutputFile &  out, T fieldPtr)
+        {
+            out.writeScalarLayer<F>(fieldPtr);
+        };
+    };
+
+    template<class F, class T>
+    class F3DFileWrite<false, F, T>
+    {
+    public:
+        static void write(Field3D::Field3DOutputFile &  out, T fieldPtr)
+        {
+            out.writeVectorLayer<float>(fieldPtr);
+        };
+    };
+
+    template <bool scalar, class T, class nF>
+    void chanToDenseF3D(  const Nb::Body * body
+                        , const nF & naiadField
+                        , const int chnIdx
+                        , Field3D::Field3DOutputFile &  out
+                        , const Nb::Vec3i & size
+                        , const Nb::Vec3i & offset
+                        , const T & def)
+    {
+        //Create a Dense Field3D Voxel Field
+        typename Field3D::DenseField<T>::Ptr fieldPtr(
+                new Field3D::DenseField<T>());
+
+        //Init names and size
+        initField(fieldPtr, body, chnIdx, size, def);
+
+        //From Naiad field to Field3D field
+        chanToField(fieldPtr, body, naiadField, offset);
+
+        //From Dense Field3D field to Field3D file
+        F3DFileWrite<scalar, T, typename Field3D::DenseField<T>::Ptr>::write(
+                out,fieldPtr);
+
+    };
+
+    template <bool scalar, class T, class nF>
+    void chanToSparseF3D( const Nb::Body * body
+                        , const nF & naiadField
+                        , const int chnIdx
+                        , Field3D::Field3DOutputFile &  out
+                        , const Nb::Vec3i & size
+                        , const Nb::Vec3i & offset
+                        , const T & def)
+    {
+        //Create a Sparse Field3D Voxel Field
+        typename Field3D::SparseField<T>::Ptr fieldPtr(
+                new Field3D::SparseField<T>());
+
+        //Init names and size
+        initField(fieldPtr, body, chnIdx, size, def);
+
+        //From Naiad field to Field3D field
+        chanToField(fieldPtr, body, naiadField, offset);
+
+        //From Sparse Field3D field to Field3D file
+        F3DFileWrite<scalar, T, typename Field3D::SparseField<T>::Ptr>::write(
+                out,fieldPtr);
+    };
+
+    template <bool scalar, class T, class nF>
+    void chanToF3D(const int type
+            , const Nb::Body * body
+            , const nF & naiadField
+            , const int chnIdx
+            , Field3D::Field3DOutputFile &  out
+            , const Nb::Vec3i & size
+            , const Nb::Vec3i & offset
+            , const T & def)
+    {
+        if (type == DENSE){
+            chanToDenseF3D<scalar, T>(body, naiadField, chnIdx, out, size,
+                    offset, def);
+        }
+        else if (type == SPARSE){
+            chanToSparseF3D<scalar, T>(body, naiadField, chnIdx, out, size
+                    , offset, def);
+        }
+    };
+
+    Nb::Vec3i getBBSize(const Nb::Vec3i & min, const Nb::Vec3i & max)
+    {
+        //Important to add a vector (1,1,1)
+        return max - min + Nb::Vec3i(1,1,1);
+    };
+
+    Nb::Vec3i getCellOffset(const Nb::Vec3i & min)
+    {
+        return min;
+    };
+
+    void getMinMax(const Nb::Body * body
+                 , Nb::Vec3i & min
+                 , Nb::Vec3i & max)
+    {
+        //Cell indices
+        int ci,cj,ck;
+
+        //Get Tile Layout
+        const Nb::TileLayout & tileLayout = body->constLayout();
+
+        //MinMax values for int
+        const int maxVal(std::numeric_limits<int>::max());
+        const int minVal(std::numeric_limits<int>::min());
+
+        //Set Vectors to opposite possible value.
+        min = Nb::Vec3i(maxVal,maxVal,maxVal);
+        max = Nb::Vec3i(minVal,minVal,minVal);
+
+        //Loop all fine tiles
+        for (int iTiles = 0; iTiles < tileLayout.fineTileCount(); ++iTiles){
+            Nb::Tile tile(tileLayout.fineTile(iTiles));
+
+            //Safety check
+            if(tile.type()==Nb::Tile::Fine) {
+                //Loop over all cells in the tile
+                for (int iCells = tile.cellsBegin();
+                        iCells < tile.cellsEnd();
+                        ++iCells){
+
+                    //Get world cell position (naiad can have cells at -1 etc)
+                    tileLayout.cijk(iCells,ci,cj,ck);
+
+                    //Perform min and max checks
+                    if (ci < min[0])
+                        min[0] = ci;
+                    if (ci > max[0])
+                        max[0] = ci;
+                    if (cj < min[1])
+                        min[1] = cj;
+                    if (cj > max[1])
+                        max[1] = cj;
+                    if (ck < min[2])
+                        min[2] = ck;
+                    if (ck > max[2])
+                        max[2] = ck;
+                }
+            }
+        }
+    };
+
+    bool IsPower2(int x)
+    {
+        return ( (x > 0) && ((x & (x - 1)) == 0) );
+    };
+} // end namespace NbF3D
---
+++ user-ops/Field3D-Channel-Write/Field3D-Channel-Write.cc
@@ -0,0 +1,219 @@
+// ----------------------------------------------------------------------------
+//
+// Field3D-Channel-Write.cc
+//
+// Copyright (c) 2011 Exotic Matter AB.  All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are
+// met:
+//
+// * Redistributions of source code must retain the above copyright notice,
+//    this list of conditions and the following disclaimer.
+//
+// * Redistributions in binary form must reproduce the above copyright notice,
+//   this list of conditions and the following disclaimer in the documentation
+//   and/or other materials provided with the distribution.
+//
+// * Neither the name of Exotic Matter AB nor its contributors may be used to
+//   endorse or promote products derived from this software without specific
+//   prior written permission.
+//
+//    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+//    "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,  INCLUDING,  BUT NOT
+//    LIMITED TO,  THE IMPLIED WARRANTIES OF  MERCHANTABILITY AND FITNESS
+//    FOR  A  PARTICULAR  PURPOSE  ARE DISCLAIMED.  IN NO EVENT SHALL THE
+//    COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+//    INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+//    BUT  NOT LIMITED TO,  PROCUREMENT OF SUBSTITUTE GOODS  OR  SERVICES;
+//    LOSS OF USE,  DATA,  OR PROFITS; OR BUSINESS INTERRUPTION)  HOWEVER
+//    CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,  STRICT
+//    LIABILITY,  OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)  ARISING IN
+//    ANY  WAY OUT OF THE USE OF  THIS SOFTWARE,  EVEN IF ADVISED OF  THE
+//    POSSIBILITY OF SUCH DAMAGE.
+//
+// ----------------------------------------------------------------------------
+
+// Naiad Base API
+#include <NbFilename.h>
+#include <NbBlock.h>
+
+// Naiad Graph API
+#include <NgBodyOp.h>
+#include <NgProjectPath.h>
+
+// Naiad Interface
+#include <Ni.h>
+
+// Naiad to Field3D interface
+#include <NbF3D.h>
+
+class Field3D_Channel_Write : public Ng::BodyOp
+{
+public:
+    Field3D_Channel_Write(const Nb::String& name)
+        : Ng::BodyOp(name) {}
+
+    virtual Nb::String
+    typeName() const
+    { return "Field3D-Channel-Write"; }
+
+    virtual void
+    stepAdmittedBody(Nb::Body*             body,
+                     Ng::NelContext&       nelContext,
+                     const Nb::TimeBundle& tb)
+    {
+        std::cerr << "Field3D-Channel Write starts" << std::endl;
+
+        //Control that the body actually has a field shape
+        if (!body->hasShape("Field"))
+            NB_WARNING("No Field Shape found.");
+
+        // Get Bodies String
+        const Nb::String bodiesStr = param1s("Body Name(s)")->eval(tb);
+        // skip bodies not listed in "Body Names" ...
+        if(!body->name().listed_in(bodiesStr))
+            return;
+
+        //Initialize Field 3D I/O
+        Field3D::initIO();
+
+        //Write output
+        Field3D::Field3DOutputFile out;
+
+        //Filename like "path/body_name.#.f3d"
+        const Nb::String bodyStr = body->name();
+        const Nb::String path = param1s("Output File Path")->eval(tb);
+        Nb::String filename = Nb::sequenceToFilename(
+            Ng::projectPath(),
+            path + Nb::String("/") + bodyStr + Nb::String(".#.f3d"),
+            tb.frame,
+            tb.timestep,
+            param1i("Frame Padding")->eval(tb)
+        );
+
+        //Create the actual write output file
+        out.create(filename.c_str());
+
+        //Get type (Dense or Sparse)
+        int type;
+        if (param1e("Body Signature")->eval(tb) == Nb::String("Dense")){
+            type = NbF3D::DENSE;
+        }
+        else if (param1e("Body Signature")->eval(tb) == Nb::String("Sparse")) {
+            type = NbF3D::SPARSE;
+
+            if (!NbF3D::IsPower2(body->constLayout().tileSize())){
+                NB_WARNING("Tile Size NOT a power of 2 in " << body->name()
+                        << ". Blame Field3D. Skipping body.");
+                return;
+            }
+
+            //Remove this once Side FX has fixed their plug-in
+            NB_WARNING("Houdini's Field3D plug-in tend to crash when loading"
+                    << " Sparse voxel fields");
+        }
+
+        // Get Channel String
+        const Nb::String channelsStr = param1s("Channel(s)")->eval(tb);
+
+        //Get Min and Max @ cell position
+        Nb::Vec3i min,max;
+        NbF3D::getMinMax(body, min ,max);
+
+        //Get the size. Remember, size in Field3D is from (0,0) to (N,N)
+        Nb::Vec3i size = NbF3D::getBBSize(min,max);
+
+        //Get offset. Should map (-N,-N) to (0,0)
+        Nb::Vec3i offset = NbF3D::getCellOffset(min);
+
+        //Access field shape
+        const Nb::FieldShape & field = body->constFieldShape();
+
+        //Loop over all channels and write to Field3D file.
+        for (int chnIdx = 0; chnIdx < field.channelCount(); ++chnIdx){
+            //Get the name of the current channel
+            Nb::String tmpChan = field.channel(chnIdx)->name();
+
+            //Default value in naiad channel. It is possible that future
+            //of Naiad will return a def value equal to the type of the channel
+            //and if that happens, this code needs some minor fixes.
+            const Nb::Vec3f nDef = field.channel(chnIdx)->defaultValue();
+
+            //If the channel is not present in the channel list passed by
+            //the user, then skip it
+            if (!tmpChan.listed_in(channelsStr))
+                return;
+
+            //Call Naiad to Field3D interface to store data.
+            switch (field.channel(chnIdx)->type()){
+                case Nb::ValueBase::FloatType: {
+                    NbF3D::chanToF3D<true>(type
+                            , body
+                            , field.constField1f(chnIdx)
+                            , chnIdx
+                            , out
+                            , size
+                            , offset
+                            , nDef[0]);
+                    break;
+                }
+                case Nb::ValueBase::Vec3fType: {
+                    //Use Imath in instead of Nb to fit Field3D
+                    Imath::V3f def(nDef[0],nDef[1],nDef[2]);
+
+                    //This struct has an operator() that will store
+                    //data in the center of each cell (even if field uses
+                    // MaC structure.
+                    NbF3D::Field3f fieldData = {field.constField3f(chnIdx,0)
+                            , field.constField3f(chnIdx,1)
+                            , field.constField3f(chnIdx,2)
+                            , body->constLayout()};
+
+                    NbF3D::chanToF3D<false>(type
+                            , body
+                            , fieldData
+                            , chnIdx
+                            , out
+                            , size
+                            , offset
+                            , def);
+                    break;
+                }
+                case Nb::ValueBase::IntType:{
+                    NbF3D::chanToF3D<true>(type
+                            , body
+                            , field.constField1f(chnIdx)
+                            , chnIdx
+                            , out
+                            , size
+                            , offset
+                            , int(nDef[0]));
+                    break;
+                }
+                default: {
+                    NB_WARNING("Can't write data from body: " << bodyStr <<
+                               " and channel: " << tmpChan <<
+                               ". Type not supported (see help)");
+                }
+            }
+        }
+
+        //Close Field3D write file
+        out.close();
+
+        std::cerr << "Field3D-Channel Write ends" << std::endl;
+    }
+};
+// ----------------------------------------------------------------------------
+
+// Register and upload this user op to the dynamics server
+
+extern "C" Ng::Op*
+NiUserOpAlloc(const Nb::String& name)
+{
+    return new Field3D_Channel_Write(name);
+}
+
+// ----------------------------------------------------------------------------
+
